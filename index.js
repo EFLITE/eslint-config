fa859ba00bba809acad2ad301c330f0cb551ddaf
EFL ITE <69828534+EFLITE@users.noreply.github.com>
2026-06-24 20:20:20 +0900

    rm quotes

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ export default antfu(
   {
     svelte: true,
     rules: {
-      'style/quotes': ['error', 'single', { avoidEscape: true, allowTemplateLiteral: 'avoidEscape' }],
       'unused-imports/no-unused-vars': 'off',
       'no-unused-vars': 'off',
       'ts/no-unused-vars': [
